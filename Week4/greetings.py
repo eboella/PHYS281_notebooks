91cca4cfac5a0ecfdd601d4a196b4cb51edd4666
Neil Drummond <n.drummond@lancaster.ac.uk>
2022-10-26 14:54:47 +0100

    Updated Week 4 examples.
    The script example now includes a class as well as a function in the
    greetings module.
    
    Added an example of representing matrices as lists of lists to the
    notebook for Week 4.

--- Week4/greetings.py
+++ Week4/greetings.py
@@ -1,23 +1,54 @@
 """
-This module holds some very sophisticated functions for displaying greetings.
+This module holds some very sophisticated code for displaying greetings.
 
 Author: Neil Drummond
 Date:   22nd October, 2022
 """
 
-def print_hello():
-    """Print a message appropriate for the start of a conversation."""
-    print("Hello!")
+import random
 
-def print_goodbye():
-    """Print a message appropriate for the end of a conversation."""
-    print("Goodbye!")
+class greeter:
+    """Class for displaying greetings messages."""
+
+    def __init__(self,name=None):
+        self.name=name
+
+    def __str__(self):
+        if self.name is None:
+            return "Generic greetings."
+        return "Greetings for {0:s}.".format(self.name)
+
+    def print_hello(self):
+        """Print a message appropriate for the start of a conversation."""
+        if self.name is None:
+            print("Hello!")
+        else:
+            print("Hello {0:s}!".format(self.name))
+
+    @staticmethod
+    def print_goodbye():
+        """Print a message appropriate for the end of a conversation."""
+        print("Goodbye!")
+
+
+def random_name():
+    """Return a randomly selected name."""
+    return random.choice(("Erlend","Haakon","Paul","Magnus","Thorfinn"))
 
 
 if __name__=="__main__":
-    print_hello()
     print("greetings.py is being executed as a script in its own right.")
-    print_goodbye()
+
+    g=greeter()
+    h=greeter(random_name())
+
+    g.print_hello()
+    print(g)
+
+    h.print_hello()
+    print(h)
+
+    greeter.print_goodbye()
 
 # The next couple of lines are just to show what happens when the module is
 # imported.  In practice you would rarely want to print anything when a module
--- Week4/script_example.py
+++ Week4/script_example.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
-# Demonstrate importing a module.
+# Demonstrate importing a module containing a class and a function.
 
+# Some different ways of dealing with imports.
 import greetings
-from greetings import print_goodbye
+from greetings import greeter as gr # Import greeter class, renaming it as gr.
 
-greetings.print_hello()
+help(greetings)      # Display help about the module.  Press "q" to quit.
+help(gr)             # Display help about the greeter class.
+help(gr.print_hello) # Display help about the print_hello method.
 
-help(greetings)
+user=gr("user")    # Equivalent to:  user=greetings.greeter("user")
+user.print_hello()
 
-help(greetings.print_hello)
+# We can use the function defined in the greetings module.
+print("Here is a random name: {0:s}".format(greetings.random_name()))
 
-print_goodbye()
+# random was imported into the greetings module, so is available here.
+print("Here is a random number: {0:.15g}".format(greetings.random.random()))
+
+user.print_goodbye()
